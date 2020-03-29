ebc798d9074fb24eef9c17c3ab9b1f98d11eb0c5
levaniCS <levanisarishvili3105@gmail.com>
2020-03-29 23:38:47 +0400

    Updates

---
+++ precache-manifest.d4a583bab62f28153d85cd81dd8c92f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8947a74ef0d44c076b288af541b65255",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/index.html"
+  },
+  {
+    "revision": "5869be0ffc0f5b174d16",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/css/main.61536174.chunk.css"
+  },
+  {
+    "revision": "95c968e16a28006b5192",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/js/2.37b7be5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/js/2.37b7be5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5869be0ffc0f5b174d16",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/js/main.7e08fcde.chunk.js"
+  },
+  {
+    "revision": "5bacb65beb857298a636",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/js/runtime-main.a05f50fe.js"
+  },
+  {
+    "revision": "5c27bcce1ebfab91952921f635737712",
+    "url": "/%20//levaniCS.github.io/covid-dashboard/static/media/cor2.5c27bcce.png"
+  }
+]);
